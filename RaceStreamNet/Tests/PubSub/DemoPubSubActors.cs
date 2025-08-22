4c6c92af67ccc2f6b418fa68ae230e4f574f402b
PatrickMisti <patrickm99@hotmail.de>
2025-08-22 18:28:38 +0800

    add test for pub sub

---
+++ RaceStreamNet/Tests/PubSub/DemoPubSubActors.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using Infrastructure.General.Message;
+using Infrastructure.General.PubSub;
+
+namespace Tests.PubSub;
+
+public sealed class BackendSubscriberTest : ReceivePubSubActor<IPubSubTopicBackend>
+{
+    public BackendSubscriberTest(IActorRef probe)
+    {
+        // Acks an Probe
+        //Receive<SubscribeAck>(ack => probe.Tell(("ack", ((Subscribe)ack.Subscribe).Topic)));
+        // Payloads an Probe
+        ReceiveAny(msg => probe.Tell(("msg back", msg)));
+    }
+}
+
+public sealed class AllSubscriberTest : ReceivePubSubActor<IPubSubTopicAll>
+{
+    public AllSubscriberTest(IActorRef probe)
+    {
+        //Receive<SubscribeAck>(ack => probe.Tell(("ack", ((Subscribe)ack.Subscribe).Topic)));
+        ReceiveAny(msg => probe.Tell(("msg all", msg)));
+    }
+}
+public class MessageTest : IPubMessage
+{
+    public string Message { get; set; } = string.Empty;
+}
--- RaceStreamNet/Tests/PubSub/PublishSubscribeTests.cs
+++ RaceStreamNet/Tests/PubSub/PublishSubscribeTests.cs
@@ -3,6 +3,7 @@ using Akka.Cluster;
 using Akka.Cluster.Tools.PublishSubscribe;
 using Akka.Configuration;
 using Akka.TestKit.Xunit2;
+using Infrastructure.General.PubSub;
 using Infrastructure.Testing;
 using Xunit;
 
@@ -44,5 +45,32 @@ public class PublishSubscribeTests() : TestKit(TestConfig)
 
         // 5) Get
         ExpectMsg<string>(msg => msg == "hello-driver", TimeSpan.FromSeconds(3));
+    }
+    
+    [Fact]
+    public async Task Should_subscribe_and_receive_messages_via_PubSub_fluent_API()
+    {
+        var cluster = Cluster.Get(Sys);
+        await cluster.JoinAsync(cluster.SelfAddress);
+        await MemberUpUtilities.WaitForMemberUp(Sys, TimeSpan.FromSeconds(5), cluster);
+
+        var probe = CreateTestProbe();
+
+        Sys.ActorOf(Props.Create(() => new AllSubscriberTest(probe.Ref)), "all-sub");
+        Sys.ActorOf(Props.Create(() => new BackendSubscriberTest(probe.Ref)), "backend-sub");
+
+        Sys.PubSub().Backend.Publish(new MessageTest{Message = "hello-backend" });
+        Sys.PubSub().All.Publish(new MessageTest { Message = "broadcast-all" });
+
+        var m1 = probe.ExpectMsg<(string kind, object msg)>(TimeSpan.FromSeconds(3));
+        var m2 = probe.ExpectMsg<(string kind, object msg)>(TimeSpan.FromSeconds(3));
+        var m3 = probe.ExpectMsg<(string kind, object msg)>(TimeSpan.FromSeconds(3));
+
+        string[] payload = [m1.kind, m2.kind, m3.kind];
+        int expectedBack = payload.Count(element => element.Contains("msg back"));
+        int expectedAll = payload.Count(element => element.Contains("msg all"));
+
+        Assert.Equal(2, expectedBack);
+        Assert.Equal(1, expectedAll);
     }
 }
