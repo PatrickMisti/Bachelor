8a6ae4e8dea020ad473342a2839838c39ca723c1
PatrickMisti <patrickm99@hotmail.de>
2025-08-25 18:46:52 +0800

    startup for ingress

---
+++ RaceStreamNet/DriverTelemetryIngress/Config/AkkaBootstrapExtension.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using Akka.Hosting;
+using Akka.Streams;
+using Infrastructure.General;
+
+namespace DriverTelemetryIngress.Config;
+
+public static class AkkaBootstrapExtension
+{
+    public static IServiceCollection ConfigureStreams(this IServiceCollection sp, AkkaHostingConfig akkaHc)
+    {
+        sp.AddAkka(akkaHc.ClusterName, config =>
+        {
+            config
+                .UseRemoteCluster(akkaHc)
+                .UseAkkaLogger()
+                .WithActors((system, registry, resolver) =>
+                {
+
+                });
+        });
+
+        // Register Materializer
+        sp.AddSingleton<IMaterializer>(ctx =>
+        {
+            var mat = ctx.GetRequiredService<ActorSystem>();
+            return mat.Materializer();
+        });
+
+        return sp;
+    }
+}
