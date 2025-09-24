384c5b295f8808f2587a53d54aa821004d0de1e6
PatrickMisti <patrickm99@hotmail.de>
2025-09-24 18:30:36 +0200

    update port add bat file

--- BachelorAkkaNet/FormulaOneAkkaNet/Program.cs
+++ BachelorAkkaNet/FormulaOneAkkaNet/Program.cs
@@ -4,7 +4,7 @@ using Infrastructure.General;
 
 string defaultUrl = "https://api.openf1.org";
 string defaultNodeName = "backend";
-int defaultPort = 5000;
+int defaultPort = 7000;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -22,13 +22,17 @@ var roles = rolesArg?
 
 if (roles.Contains(ClusterMemberRoles.Controller.ToStr()) && roles.Count == 1)
     defaultPort = 6000;
+else if (roles.Contains(ClusterMemberRoles.Backend.ToStr()) && roles.Count == 1)
+    defaultPort = 5000;
+else if (roles.Count > 1)
+    defaultPort = 5000;
 
-var akkaHc = new AkkaConfig
-{
-    Port = PortChecker.CheckPort(defaultPort),
-    Role = roles.Count == 1 ? roles.First() : defaultNodeName,
-    Roles = roles
-};
+    var akkaHc = new AkkaConfig
+    {
+        Port = PortChecker.CheckPort(defaultPort),
+        Role = roles.Count == 1 ? roles.First() : defaultNodeName,
+        Roles = roles
+    };
 
 builder.CreateLoggingAdapter();
 
---
+++ BachelorAkkaNet/FormulaOneAkkaNet/start.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+setlocal
+
+echo Starte Akka.NET Cluster Nodes...
+
+:: 1. Node
+start "Backend" dotnet run --project .\FormulaOneAkkaNet.csproj --launch-profile "Backend"
+
+:: 2. Node
+start "Controller" dotnet run --project .\FormulaOneAkkaNet.csproj --launch-profile "Controller"
+
+:: 3. Node
+start "Ingress" dotnet run --project .\FormulaOneAkkaNet.csproj --launch-profile "Ingress"
+
+echo Alle Nodes gestartet.
+endlocal
