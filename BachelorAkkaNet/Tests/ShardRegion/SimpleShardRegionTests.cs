5a71ccd869320bfa8485598d60d6f6cd9862d984
PatrickMisti <patrickm99@hotmail.de>
2025-09-24 18:43:34 +0200

    init simple shard tests

---
+++ BachelorAkkaNet/Tests/ShardRegion/SimpleShardRegionTests.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Event;
+using Akka.TestKit.Xunit2;
+using Xunit.Abstractions;
+
+namespace Tests.ShardRegion;
+
+public class SimpleShardRegionTests(ITestOutputHelper helper) : TestKit(TestConfig,helper)
+{
+    private static readonly string TestConfig = """
+                                                akka.stdout-loglevel = Warning
+                                                akka.loggers = ["Akka.TestKit.TestEventListener, Akka.TestKit"]
+                                                akka.loglevel = Info
+                                                """;
+
+    private ILoggingAdapter _log => Sys.Log;
+}
