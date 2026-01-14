03462571f554924107bf584ca4e0f5618de7844d
PatrickMisti <patrickm99@hotmail.de>
2026-01-14 16:17:12 +0100

    update version

---
+++ BachelorAkkaNet/SystemConsoleTests/DemoActor.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+
+namespace SystemConsoleTests;
+
+public class DemoActor : ReceiveActor
+{
+    public DemoActor()
+    {
+        Receive<string>(msg =>
+        {
+            Console.WriteLine($"DemoActor received message: {msg}");
+        });
+    }
+
+    public static Props Prop()
+    {
+        return Props.Create(() => new DemoActor());
+    }
+}
--- BachelorAkkaNet/SystemConsoleTests/Program.cs
+++ BachelorAkkaNet/SystemConsoleTests/Program.cs
@@ -7,7 +7,6 @@ using Infrastructure.PubSub;
 using Infrastructure.PubSub.Messages;
 using SystemConsoleTests;
 
-
 string host = "localhost";
 var port = 0;
 
@@ -19,13 +18,27 @@ var config = ConfigurationFactory
 
 using var system = ActorSystem.Create("cluster-system", config);
 
-var actor = system.ActorOf(ClientApiActor.Prop());
-var cluster = Cluster.Get(system);
-await MemberEventUp.WaitForMemberUp(system, TimeSpan.FromSeconds(30), cluster);
+async Task Test1()
+{
+    var actor = system.ActorOf(ClientApiActor.Prop());
+    var cluster = Cluster.Get(system);
+    await MemberEventUp.WaitForMemberUp(system, TimeSpan.FromSeconds(30), cluster);
 
-DistributedPubSub.Get(system);
-await Task.Delay(1000);
+    DistributedPubSub.Get(system);
+    await Task.Delay(1000);
 
-system.PubSub().Ingress.Publish(new IngressSessionRaceMessage(9158));
+    system.PubSub().Ingress.Publish(new IngressSessionRaceMessage(9158));
+
+    await Task.Delay(Timeout.Infinite);
+}
+
+
+
+//await Test1();
+
+var actor = system.ActorOf(DemoActor.Prop());
+
+actor.Tell("Test");
 
 await Task.Delay(Timeout.Infinite);
+
